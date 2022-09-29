bd4213980951dea643966a358d8e1df13b6971f4
Richards Cedeño <chards.rjcov@gmail.com>
2022-09-29 01:14:41 -0500

    Subiendo calculadora-js

---
+++ index.js
@@ -0,0 +1,30 @@
+const display = document.querySelector('#display')
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((item) => {
+  item.onclick=()=>{
+    if(item.id === "clear") {
+      display.innerText=""
+    } else if(item.id === "backspace") {
+      let string = display.innerText.toString()
+      display.innerText = string.substr(0,string.length-1)
+    } else if(display.innerText != "" && item.id === "equal") {
+      display.innerText = eval(display.innerText)
+    } else if(display.innerText == "" && item.id == "equal") {
+      display.innerText = "Null"
+      setTimeout(()=>(display.innerText=""),1000)
+    } else {
+      display.innerText += item.id
+    }
+  }
+})
+
+const toggleBtn = document.querySelector('.theme-toggle')
+const calculator = document.querySelector('.calculator')
+
+// let isDark = true
+toggleBtn.onclick=()=> {
+  calculator.classList.toggle('dark')
+  // toggleBtn.classList.toogle("active")
+  // isDark = !isDark
+}
